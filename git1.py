da171db3d4648e9edaef5f4d2363aecae9cce87b
knutpython <85614197+knutpython@users.noreply.github.com>
2021-06-13 22:34:24 +0800

    1
    111

---
+++ git1.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# @Author   : knut
+# @Time     : 2021/6/13 10:59
+
+myList={1,2,3,4}
+myDictionary={"key":"value","key2":"value"}
+
+myPhones={"Iphone x":1000,"Sumsang S9":900}
+
+print(myPhones)
+# access dic keys
+# str(Iphone_Price) 换算成str，两个str才能相加
+Iphone_Price=myPhones["Iphone x"]
+print("IPhone price:"+str(Iphone_Price))
+
+# change key values
+myPhones["Iphone x"]=500
+print(myPhones)
+
+# 清除
+myPhones.clear()
+print(myPhones)
