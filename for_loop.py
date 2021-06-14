350e34b5910ce3058465b0f66e9ee49e1be9b1b6
knutpython <lh49585404@gmail.com>
2021-06-14 12:43:13 +0800

    loop

---
+++ for_loop.py
@@ -0,0 +1,17 @@
+my_list=["jake","tom","xiao li"]
+
+# 打印出list中所有的数据；'name'可以随便写
+for name in my_list:
+    print(name)
+# 打印出0-10
+for number in range(10):
+    print(number)
+# 打印出10到20
+for number in range(10,21):
+    print(number)
+# 打印出每一个字母,每行一个字母
+for letter in 'inty':
+    print(letter)
+# 每个字母前加上each letter is
+for letter in 'inty':
+    print("each letter is"+letter)
